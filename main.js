74d12bad174ace6866506ae0d414cb354ccfbaef
Jcarbonell04 <61067221+Jcarbonell04@users.noreply.github.com>
2020-02-26 14:38:02 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Event Listener
+document.getElementById('Androidbtn').addEventListener('click', clickHandler);
+
+//Event Functions
+function clickHandler() {
+    document.getElementById("img1").src = "images/Android-vs-apple.jpg";
+
+}
