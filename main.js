32a2e2e5d02ed767314e832f5bd579583086448f
Jcarbonell04 <61067221+Jcarbonell04@users.noreply.github.com>
2020-02-26 15:18:56 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,51 @@
-'use strict';
+  
+//Android Vs Apple Example
 
-// Event Listener
-document.getElementById('Androidbtn').addEventListener('click', clickHandler);
+//When Button is Clicked
+document.getElementById('androidBtn').addEventListener('click', setAndroid);
+document.getElementById('appleBtn').addEventListener('click', setApple);
+
 
 //Event Functions
-function clickHandler() {
-    document.getElementById("img1").src = "images/Android-vs-apple.jpg";
+function setAndroid() {
+    //change main photo
+    document.getElementById('img1').src = "images/android-logo.jpg";
 
+    //change link name
+    document.getElementById('link1').innerHTML = 'Android Home';
+
+    //change background color
+    document.body.style.backgroundColor = '#a4c93b';
+
+    //update link
+    document.getElementById('link1').href = 'https://www.android.com/';
+
+    //change body background
+    document.body.style.color = '#a4c93b';
+
+    document.getElementById('appleBtn').classList.remove('apple-btn');
+
+    document.getElementById('androidBtn').classList.add('android-btn')
+}
+
+//Event Functions
+function setApple() {
+    //change main photo
+    document.getElementById('img1').src = "images/apple-logo.jpg";
+
+    //change link name
+    document.getElementById('link1').innerHTML = 'Apple Home';
+
+    //change background color
+    document.body.style.backgroundColor = '#b6bcca';
+
+    //update link
+    document.getElementById('link1').href = 'https://www.apple.com/';
+
+    //change body background
+    document.body.style.color = '#b6bcca';
+
+    document.getElementById('appleBtn').classList.add('apple-btn');
+
+    document.getElementById('androidBtn').classList.remove('android-btn')
 }
